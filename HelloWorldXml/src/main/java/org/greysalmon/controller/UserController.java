e9ec82c7929feb741a6eb763e0b18c8a9b8dc4ad
Marek Gulka <marek.gulka@atos.net>
2018-07-06 19:08:45 +0200

    29th minute of 8th video, I need to set up db and test the app

--- HelloWorldXml/src/main/java/org/greysalmon/controller/UserController.java
+++ HelloWorldXml/src/main/java/org/greysalmon/controller/UserController.java
@@ -2,31 +2,32 @@ package org.greysalmon.controller;
 
 import java.util.Map;
 
+import org.greysalmon.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(value = "/user")
 public class UserController {
 
-	@RequestMapping(value = "/user/register", method = RequestMethod.GET)
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String register(Map<String, Object> map) {
 		// map.put("title", "User Registration");
 		return "user/register";
 	}
 
 	// method for handling the user data
-	@RequestMapping(value = "/user/details", method = RequestMethod.POST)
-	public String detils(@RequestParam("fName") String fName, @RequestParam("lName") String lName,
-			@RequestParam("email") String email, @RequestParam("mobile") String mobile, Map<String, Object> map) {
+	@RequestMapping(value = "/details", method = RequestMethod.POST)
+	public String detils(User user, Map<String, Object> map) {
 
 		// putting all values form the form to a map that will be passed to the
 		// jsp
-		map.put("fName", fName);
-		map.put("lName", lName);
-		map.put("email", email);
-		map.put("mobile", mobile);
+		map.put("fName", user.getfName());
+		map.put("lName", user.getlName());
+		map.put("email", user.getEmail());
+		map.put("mobile", user.getMobile());
 		return "user/details";
 	}
 }
---
+++ HelloWorldXml/src/main/java/org/greysalmon/model/User.java
@@ -0,0 +1,38 @@
+package org.greysalmon.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+	
+	private Long id; // primary key in db
+	private String fName;
+	private String lName;
+	private String email;
+	private String mobile;
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+}
